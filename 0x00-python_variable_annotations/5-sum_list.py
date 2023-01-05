ba143b9db3fb4c29b745eea05de496c25b8ca089
thala254 <thalamine@gmail.com>
2023-01-06 00:16:52 +0300

    updated files to conform to pep8 styling

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,7 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    a type-annotated function which takes a list input_list of 
+    a type-annotated function which takes a list input_list of
     floats as argument and returns their sum as a float
     """
     return float(sum(input_list))

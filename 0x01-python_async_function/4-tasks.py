f9ca2481f0a76e99d3ce77eea91925ebefa5dfc8
thala254 <thalamine@gmail.com>
2023-01-10 00:57:46 +0300

    corrected task files to conform to pep8 styling

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    'Executes task_wait_random n times with the specified max_delay and returns the list of all the delays (float values) in ascending order
+    Executes task_wait_random n times with the specified max_delay and returns
+    the list of all the delays (float values) in ascending order
     """
-    delays = await asyncio.gather(*[task_wait_random(max_delay) for i in range(n)])
+    delays = await asyncio.gather(
+      *[task_wait_random(max_delay) for i in range(n)])
     return sorted(delays)
